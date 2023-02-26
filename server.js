8c2119078238dd66e735ba7f56a6bb4556129ce3
Puram Sai Varshitha <21wh5a1204@bvrithyderabad.edu.in>
2023-02-26 14:15:03 +0530

    connected to MONGO DB

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+const str = process.env.MONGO_CONNECTION_STRING;
+mongoose.set('strictQuery', true);
+mongoose.connect(str, function(err){
+    if(err)
+        console.error(err);
+    else{
+        console.log("DB CONNECTED");
+        
+        app.listen(3000, function(){
+            console.log("SERVER running in http://localhost:3000")
+        })
+    }
+});
 
 app.use(express.static("frontend"))
 
@@ -8,6 +24,3 @@ app.get("/", function(req,res){
     res.sendFile(__dirname + "/frontend/index.html")
 })
 
-app.listen(3000, function(){
-    console.log("SERVER running in http://localhost:3000")
-})
